f059f70109fa3a00f7142cca498bb7c98e7cdea8
Vincent Bernat <vincent@bernat.im>
2014-08-13 18:31:17 +0200

    cisco-puppet: when running with dynamips, no CatOS present
    So, no VLAN...

--- lab-cisco-puppet/puppet/manifests/site.pp
+++ lab-cisco-puppet/puppet/manifests/site.pp
@@ -1,25 +1,9 @@
 node "cisco.local" {
 
-  vlan { "99":
-    description => "Sales department"
-  }
-  vlan { "100":
-    description => "Marketing department"
-  }
-
   interface { "FastEthernet 0/1":
-      description => "dummy interface",
-      mode => trunk,
-      allowed_trunk_vlans => "99, 100"
-  }
-
-  interface { "Vlan99":
-    description => "Sales department",
-    ipaddress => "192.168.14.1/24"
-  }
-  interface { "Vlan100":
-    description => "Marketing department",
-    ipaddress => "192.168.15.1/24"
+    description => "dummy interface",
+    ipaddress => "192.168.14.1/24",
+    ensure => present
   }
 
 }
