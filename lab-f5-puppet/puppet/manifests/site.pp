aa4e1fcdd985de94711a5fe081bb82a66c143bf3
Vincent Bernat <vincent@bernat.im>
2014-08-15 10:33:34 +0200

    f5-puppet: fix for MODULES-424
    See:
     https://tickets.puppetlabs.com/browse/MODULES-424

--- lab-f5-puppet/puppet/manifests/site.pp
+++ lab-f5-puppet/puppet/manifests/site.pp
@@ -1,12 +1,26 @@
 node "f5.local" {
 
-  f5_pool { 'webfront':
+  f5_pool { '/Common/webfront':
     ensure => present,
     action_on_service_down => 'SERVICE_DOWN_ACTION_NONE',
     lb_method => 'LB_METHOD_ROUND_ROBIN',
+    allow_nat_state => 'STATE_ENABLED',
+    allow_snat_state => 'STATE_ENABLED',
+    minimum_active_member => '1',
+    minimum_up_member => '0',
     member => {
-      '10.10.0.1:80' => {},
-      '10.10.0.2:80' => {}
+      '/Common/10.10.0.1:80' => {
+        "connection_limit"=>"0",
+        "dynamic_ratio"=>"1",
+        "priority"=>"0",
+        "ratio"=>"1"
+      },
+      '/Common/10.10.0.2:80' => {
+        "connection_limit"=>"0",
+        "dynamic_ratio"=>"1",
+        "priority"=>"0",
+        "ratio"=>"1"
+      }
     }
   }
 
