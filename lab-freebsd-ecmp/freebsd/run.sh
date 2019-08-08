0243478d580a3894e89109a358fefa457474b6e6
Vincent Bernat <vincent@bernat.ch>
2019-08-08 09:20:57 +0200

    freebsd-ecmp: more experiments

--- lab-freebsd-ecmp/freebsd/run.sh
+++ lab-freebsd-ecmp/freebsd/run.sh
@@ -31,9 +31,14 @@ route add -net 0.0.0.0/0 10.0.2.2
 ifconfig lo0 inet 203.0.113.14/32 alias
 ifconfig vtnet1 inet 192.0.2.11/31
 ifconfig vtnet2 inet 192.0.2.13/31
+ifconfig vtnet2 inet 203.0.113.14/32 alias
 ifconfig vtnet3 inet 192.0.2.15/31
-route add -net 203.0.113.15/32 192.0.2.12
-route add -net 203.0.113.15/32 192.0.2.14
+ifconfig vtnet3 inet 203.0.113.14/32 alias
+route add -net 203.0.113.15/32 192.0.2.12 -ifa 203.0.113.14 -ifp vtnet2
+route add -net 203.0.113.15/32 192.0.2.14 -ifa 203.0.113.14 -ifp vtnet3
 
 # RADIX_MPATH option is not enabled by default...
 #  svn checkout https://svn.FreeBSD.org/base/release/11.2.0 /usr/src
+
+# Tentative for source address selection are breaking stuff (next-hop
+# interface and source address selection are tied together).
